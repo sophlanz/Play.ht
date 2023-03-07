a13dda581b99dc8fea89d09752368f848b75e44e
Sophia Lanzendorf <sophialanzendorf@gmail.com>
2023-03-07 13:30:06 -0500

    make api call to generate selected voice to selected text. Make another call to recieve the audio

---
+++ recieve-audio.py
@@ -0,0 +1,15 @@
+import os,requests,json
+from dotenv import load_dotenv
+
+load_dotenv()
+SECRET_KEY = os.getenv('SECRET_KEY')
+API_ID = 'CDwIWESGJbhDXmUqO17BnhiJdiE2'
+headers =  {
+        'Authorization': SECRET_KEY,
+        'X-User-ID': API_ID,
+        "Content-Type":"application/json"
+    }
+url = 'https://play.ht/api/v1/articleStatus?transcriptionId=-NPxJoDbwa2d4QXCGFod'
+payload = json.dumps({"status":"CREATED","transcriptionId":"-NPxHE714ljH9II8tTEe","contentLength":2,"wordCount":10})
+response = requests.request("GET",url, headers=headers,data=payload)
+print(response.text)
---
+++ text-to-speech.py
@@ -0,0 +1,26 @@
+import os,requests,json
+from dotenv import load_dotenv
+
+load_dotenv()
+SECRET_KEY = os.getenv('SECRET_KEY')
+API_ID = 'CDwIWESGJbhDXmUqO17BnhiJdiE2'
+#How the request body looks like
+
+payload = json.dumps({
+  "voice": "en-US-AriaNeural",
+  "content": [
+   "Hi Sophia",
+   "woah woah woah woah woah woah woah woah woah woah"
+  ],
+   "title": "Testing public api convertion",
+     "narrationStyle":"cheerful"
+})
+url = "https://play.ht/api/v1/convert"
+
+headers =  {
+        'Authorization': SECRET_KEY,
+        'X-User-ID': API_ID,
+        "Content-Type":"application/json"
+    }
+response = requests.request("POST", url, headers=headers, data=payload)
+print(response.text)
